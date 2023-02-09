0cad53139ad0fc042684d4e67c8c756d0316f003
Marples <h00740@uniper.energy>
2023-02-09 13:22:30 +0000

    Implemented conventional package project structure and template rendering handlers.

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/hmarp/basic-web-app/pkg/handlers"
+)
+
+const portNumber = ":8080"
+
+// main is the main application function
+func main() {
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
+
+	fmt.Printf("Starting application on port %s", portNumber)
+
+	http.ListenAndServe(portNumber, nil)
+}
